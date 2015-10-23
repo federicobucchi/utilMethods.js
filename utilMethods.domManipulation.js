f202417ea5b4fed251d73b0c351dbd6eba73af6c
Federico Bucchi <b.federico@gmail.com>
2015-10-23 15:51:16 -0700

    start domManipulation extension

---
+++ utilMethods.domManipulation.js
@@ -0,0 +1,16 @@
+(function(utilMethods) {
+  utilMethods.extensions += ' + domManipulation';
+
+  utilMethods.domManipulation = {
+    elClass: function(el) {
+
+      return {
+        remove: function() {
+          // build the remove element function
+        }
+      }
+
+    }
+  }
+
+})(utilMethods);
