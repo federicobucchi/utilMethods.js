b9530e77c0755d49bc012e1beba1f8c4a9f4576b
Federico Bucchi <b.federico@gmail.com>
2015-10-27 10:15:33 -0700

    add more spec files

--- spec/tests/utilMethods.domManipulation_spec.js
+++ spec/tests/utilMethods.domManipulation_spec.js
@@ -1,8 +1,9 @@
 var fs = require('fs')
 var utilMethods = fs.readFileSync('utilMethods.js','utf-8');
+var domManipulation = fs.readFileSync('utilMethods.domManipulation.js','utf-8');
 
 describe("TEST", function() {
   it("TEST", function() {
-    console.log(utilMethods);
+    console.log(domManipulation);
   });
 });
